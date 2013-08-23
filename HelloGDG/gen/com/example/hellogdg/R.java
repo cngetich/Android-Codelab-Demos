e4771ee1213d1348b2f573800b23538cebc96d97
Karthik K <karthikthephoenix@gmail.com>
2013-08-23 22:19:44 +0530

    Final Sample Commit
    Fixed the code with working models.

--- HelloGDG/gen/com/example/hellogdg/R.java
+++ HelloGDG/gen/com/example/hellogdg/R.java
@@ -25,11 +25,11 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080005;
-        public static final int addBtn=0x7f080004;
-        public static final int num1=0x7f080001;
-        public static final int num2=0x7f080002;
+        public static final int addBtn=0x7f080003;
+        public static final int amount1=0x7f080001;
+        public static final int amount2=0x7f080002;
+        public static final int sumResult=0x7f080004;
         public static final int textView1=0x7f080000;
-        public static final int textView2=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- HelloGDG/src/com/example/hellogdg/MainActivity.java
+++ HelloGDG/src/com/example/hellogdg/MainActivity.java
@@ -1,21 +1,58 @@
 package com.example.hellogdg;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	EditText amount1;
+	EditText amount2;
+	TextView sumResult;
+
+	Button calculate;
+
+	double x = 0;
+	double y = 0;
+	double z = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		addTheNumbers();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private void addTheNumbers() {
+		// TODO Auto-generated method stub
+		amount1 = (EditText) findViewById(R.id.amount1);
+		amount2 = (EditText) findViewById(R.id.amount2);
+		calculate = (Button) findViewById(R.id.addBtn);
+		sumResult = (TextView) findViewById(R.id.sumResult);
+
+		calculate.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				System.out.println("Inside onClick");
+				addition();
+			}
+
+			private void addition() {
+				// TODO Auto-generated method stub
+				System.out.println("Inside Addition:" + x + "and" + y);
+				x = Double.parseDouble(amount1.getText().toString());
+				y = Double.parseDouble(amount2.getText().toString());
+				z = x + y;
+				System.out.println("Result:" + z);
+				sumResult.setText(Double.toString(z));
+			}
+		});
+
 	}
+
 }
